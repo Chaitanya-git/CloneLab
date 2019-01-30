6c6775990b8cc73cc347dc914a634ad6c46b59e9
Chaitanya <chaitanya2.1999@gmail.com>
2019-01-30 18:38:28 +0530

    Convert script into a pip package
    Running clonelab as a simple python script makes it more cumbersome to
    run clonelab from other directories. Installing it as a pip package
    should make it easier to install and uninstall clonelab and manage
    dependencies.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clonelab-chaitanya",
+    version="0.0.1",
+    author="Chaitanya",
+    author_email="chaitanya2.1999@gmail.com",
+    description="A python utility to clone groups and subgroups from gitlab",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chaitanya-git/clonelab",
+    packages=setuptools.find_packages(),
+    scripts=['scripts/clonelab'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3",
+        "Operating System :: POSIX",
+    ],
+)
