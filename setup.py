889e09da19d2a9bfaf13b7b97a812c8a4bacc41f
Chaitanya <chaitanya2.1999@gmail.com>
2019-03-01 20:21:25 +0530

    Check response code returned by gitlab for errors

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clonelab",
-    version="0.0.3",
+    version="0.0.4",
     author="Chaitanya",
     author_email="chaitanya2.1999@gmail.com",
     description="A python utility to clone groups and subgroups from gitlab",
